149261d22cb2a49056187220b8341c8a022883d0
Timothy Mukaibo <timothy@mukaibo.com>
2014-10-26 16:36:41 +1100

    Add invoice items to upcoming invoice
    Return any invoice items you have created for a customer, instead
    of a mock.
    
    Conflicts:
    	lib/stripe_mock/request_handlers/invoices.rb

--- lib/stripe_mock/request_handlers/invoices.rb
+++ lib/stripe_mock/request_handlers/invoices.rb
@@ -64,10 +64,14 @@ module StripeMock
         raise Stripe::InvalidRequestError.new("No upcoming invoices for customer: #{customer[:id]}", nil, 404) if customer[:subscriptions][:data].length == 0
 
         most_recent = customer[:subscriptions][:data].min_by { |sub| sub[:current_period_end] }
-        invoice_item = get_mock_subscription_line_item(most_recent)
+
+        items_for_invoice = get_upcoming_invoice_items(customer[:id])
+        if items_for_invoice.empty?
+          items_for_invoice = [ get_mock_subscription_line_item(most_recent) ]
+        end
 
         id = new_id('in')
-        invoices[id] = Data.mock_invoice([invoice_item],
+        invoices[id] = Data.mock_invoice(items_for_invoice,
           id: id,
           customer: customer[:id],
           subscription: most_recent[:id],
@@ -77,6 +81,16 @@ module StripeMock
       end
 
       private
+
+      # Gets the (uncharged) invoice items for an upcoming invoice. If the invoice is nil,
+      # then it hasn't yet been charged, which means it will be charged as part of the next
+      # subscription invoice created event.
+      def get_upcoming_invoice_items(customer_id)
+        upcoming_items = []
+        items = invoice_items.select { |k,v| v[:customer] == customer_id && v[:invoice].nil? }
+        items.keys.each { |k| upcoming_items << items[k] }
+        upcoming_items
+      end
 
       def get_mock_subscription_line_item(subscription)
         Data.mock_line_item(
